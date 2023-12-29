30030fe5f3131859c371d2770eb8d6d40b9efdf2
nikit <nikita.milanov2@mail.ru>
2023-12-29 18:27:32 +0500

    hotfix(api): realized ErrorController

---
+++ Brusnika.Api/Controllers/Common/ErrorController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Brusnika.Api.Controllers.Common;
+
+[ApiExplorerSettings(IgnoreApi = true)]
+public class ErrorsController : ControllerBase
+{
+    [Route("/error")]
+    public IActionResult Error()
+    {
+        if (HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error is { } exception)
+            return Problem(title: exception.Message);
+        
+        return Problem("Something went wrong.");
+    }
+}
